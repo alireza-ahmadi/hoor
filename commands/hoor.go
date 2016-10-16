39e7e85bb0e3176c396fb6bdba448ae527cbdcc3
Alireza Ahmadi <a.ahmadi94@gmail.com>
2016-10-16 20:10:29 +0330

    Setup code structure

---
+++ commands/hoor.go
@@ -0,0 +1,8 @@
+// Package commands define command-line interface for Hoor, current implementation
+// is based on the Cobra package.
+package commands
+
+// Setup adds all child commands to the root command and sets flags appropriately
+func Setup() {
+
+}
---
+++ main.go
@@ -0,0 +1,9 @@
+// Hoor is a command line utility for adding Shamsi date feature to Hugo based websites.
+// Learn more by running `hoor` command in your command line.
+package main
+
+import "github.com/alireza-ahmadi/hoor/commands"
+
+func main() {
+	commands.Setup()
+}
